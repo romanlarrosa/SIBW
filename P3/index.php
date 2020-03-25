15a3d387bd3793b5ef33eaf5e859367c9edb2a32
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-03-25 17:51:09 +0100

    UPDATE

--- P3/index.php
+++ P3/index.php
@@ -4,6 +4,14 @@
     $loader = new \Twig\Loader\FilesystemLoader('plantillas');
     $twig = new \Twig\Environment($loader);
 
+    
+    
+    $mysqli = new mysqli("localhost", "web", "web", "SIBW");
+    if ($mysqli->connect_errno) {
+        print ("FALLO AL CONECTAR: " . $mysqli->connect_error);
+    }
+    
+
     echo $twig->render('portada.html', []);
     
     
