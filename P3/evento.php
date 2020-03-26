8a29d181d50c307bd3fdbbd3558db426871e924b
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-03-26 02:25:00 +0100

    Updated

--- P3/evento.php
+++ P3/evento.php
@@ -8,17 +8,26 @@
   $twig = new \Twig\Environment($loader);
   
   $evento = array('nombre' => "ERROR404", 'contenido' => "No encontrado");
+  $fotos = array();
+  $estilo = "css/estilos.css";
   
   if (isset($_GET['ev'])) {
     $idEv = $_GET['ev'];
+    $mysqli = conectar();
+    $evento = getEvento($idEv, $mysqli);
+    $fotos = getFotosEvento($idEv, $mysqli);
   } else {
     $idEv = -1;
   }
 
-  $mysqli = conectar();
-  $evento = getEvento($idEv, $mysqli);
-  $fotos = getFotosEvento($idEv, $mysqli);
+  if (isset($_GET['sty']) && $_GET['sty'] == 1) {
+    $estilo = "css/estilo_imprimir.css";
+  }
+
+
+
+  
   
   //print("Algo estoy haciendo " . $evento['contenido']);
-  echo $twig->render('evento.html', ['evento' => $evento, 'fotos' => $fotos]);
+  echo $twig->render('evento.html', ['evento' => $evento, 'fotos' => $fotos, 'estilo' => $estilo, 'ev' => $idEv]);
 ?>
