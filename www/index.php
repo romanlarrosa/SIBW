945179ecbc8f6325601d2dc777d0b28e762e4988
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-05-13 00:22:10 +0200

    Formated repository

--- www/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require_once "/usr/local/lib/php/vendor/autoload.php";
-
-  $loader = new \Twig\Loader\FilesystemLoader('templates');
-  $twig = new \Twig\Environment($loader);
-  
-  echo $twig->render('index.html', []);
-?>
